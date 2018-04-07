86ff656fc45e5d5ae7ff0657fdb9a05c8efe41fb
alumno <dislila@hotmail.com>
2018-04-07 13:21:51 -0500

    Initial commit

---
+++ src/app/autos/autos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autos',
+  templateUrl: './autos.component.html',
+  styleUrls: ['./autos.component.css']
+})
+export class AutosComponent implements OnInit {
+  public marca: string
+  public modelo: string
+  public anio: number
+  public marcas: Array<string>
+  public esVisible: boolean = false
+
+  constructor() {
+    this.marca = "Nissan"
+    this.modelo = "Skyline"
+    this.anio = 2012
+    this.marcas = ['Honda', 'Toyota', 'Mazda', 'Nissan']
+  }
+
+  ngOnInit() {
+  }
+  public muestraMarcas() {
+    this.esVisible = true
+  }
+  public escondeMarcas() {
+    this.esVisible = false
+  }
+}
